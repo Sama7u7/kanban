19b8810a75cd2eb48e5847438f132823e93556ee
SamayoaChamba <L20270788@tuxtla.tecnm.mx>
2026-02-20 14:31:05 -0600

    Inicio de sesion y controlador creado, pero con error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,37 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
-    public function login(Request $request)
+    public function authenticate(Request $request): RedirectResponse
     {
         $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+            'email' => ['required', 'email'],
+            'password' => ['required'],
         ]);
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            $user = Auth::user();
-            if ($user->role === 'admin') {
-            // Si es admin, lo mandamos al dashboard de admin.
-            return redirect()->intended('admin');
-            }
+
+            return redirect()->intended('tasks');
         }
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            $user = Auth::user();
-            if ($user->role === 'user') {
-            // Si es admin, lo mandamos al dashboard de admin.
-            return redirect()->intended('user');
-            }
-        }
+
         return back()->withErrors([
-            'email' => 'Las credenciales no son correctas.',
-        ]);
+            'email' => 'Las credenciales que introduciste son incorrectas.',
+        ])->onlyInput('email');
     }
 
     public function logout(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {return view('welcome');});
-Route::get('/dashboard', function () {return view('dashboard');});
+// Route::get('/dashboard', function () {return view('dashboard');});
 Route::resource('tasks', TaskController::class);
 Route::patch('tasks/{task}/status', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
 
-
+Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
